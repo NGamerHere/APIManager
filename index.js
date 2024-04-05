642a805fd28349de1b39b27d001cbd3a8eb9c07f
datta <codingdatta@gmail.com>
2024-04-05 14:00:29 +0530

    set up the basic get and post request route

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 const express=require("express");
+const bodyParser=require("body-parser")
 
 const app=express();
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+
+const ds={
+   name:"datta"
+}
+const routing=[];
 
 app.get("/",(req, res)=>{
-   res.send("hello there")
+    if (routing.length===0){
+        return res.send("no data")
+    }
+   res.send(routing)
 })
 
 
-app.post("",(req, res)=>{
 
+
+app.post("/",(req, res)=>{
+    console.log(req.body.route)
+    routing.push(req.body)
+
+  res.send("post request")
 })
 
 app.listen(4000,()=>{
